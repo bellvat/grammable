2e8ee161c8321dd0cfd7061d57275a67cc88a37f
bellvat <msirathanarun@gmail.com>
2017-09-08 15:46:31 +0700

    Verified that users who update, edit and destroy grams are the ones who created it

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,5 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :create]
+  before_action :authenticate_user!, only:[:new, :create, :update, :edit, :destroy]
   #before_action :require_authorized_for_gram, only: [:show]
   
   def index
@@ -26,11 +26,14 @@ class GramsController < ApplicationController
   def edit
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+
+    return render_not_found(:forbidden) if @gram.user != current_user
   end
 
   def update
     @gram= Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
 
     @gram.update_attributes(gram_params)
     if @gram.valid?
@@ -43,7 +46,7 @@ class GramsController < ApplicationController
   def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
-
+    return render_not_found(:forbidden) if @gram.user != current_user
     @gram.destroy
     redirect_to root_path
   end
@@ -56,8 +59,8 @@ class GramsController < ApplicationController
     #end
   #end
 
-  def render_not_found
-      render plain: 'Not found', status: :not_found
+  def render_not_found(status=:not_found)
+      render plain: "#{status.to_s.titleize}", status: status
   end
 
 
