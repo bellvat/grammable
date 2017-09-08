08863e59cd76b43e9dadb6e84598febfc89ed6e9
bellvat <msirathanarun@gmail.com>
2017-09-08 12:35:52 +0700

    Added show grams page and valid grams check

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,11 +1,19 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only:[:new, :create]
+  #before_action :require_authorized_for_gram, only: [:show]
   
   def index
   end
 
   def new
     @gram = Gram.new
+  end
+
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
   end
 
   def create
@@ -17,7 +25,15 @@ class GramsController < ApplicationController
     end
   end
 
+
+
   private
+
+  #def require_authorized_for_gram
+   # if @gram.user_id != current_user
+    #  render plain: "Unauthorized", status: :Unauthorized
+    #end
+  #end
 
   def gram_params
     params.require(:gram).permit(:message)
