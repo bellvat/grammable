316ecbf09434c804904d9872456cd901213c4896
bellvat <msirathanarun@gmail.com>
2017-11-02 13:42:17 -0700

    Moved database.yml to gitignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.find_or_create_by(:email => 'test2@gmail.com', :password => "test", :password_confirmation => "test")
